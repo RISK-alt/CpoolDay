8562a5ba5d1b3c43752bdaa610330689543eec5a
RISK <81443404+RISK-alt@users.noreply.github.com>
2024-04-17 10:13:46 +0000

    dd

--- main.c
+++ main.c
@@ -1,15 +1,15 @@
 void print_char(char i);
+int hh
 
-void my_print_ascii(void) {
+void my_print_n_ascii(int 5) {
     int i;
     for (i = 33; i < 126; i++)
     {
         print_char((char)i);
-    }
+        }
 }
-
-
-
+    
+    
 int main(void)
 {
     my_print_ascii();
@@ -22,4 +22,3 @@ return 0;
 // char type name () {}
 // print_char(char c);
 // print_number(int number);
-
