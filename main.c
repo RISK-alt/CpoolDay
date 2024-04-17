a11b02eea47ff77853c8e0170d22f810102b3af5
RISK <81443404+RISK-alt@users.noreply.github.com>
2024-04-17 09:37:37 +0000

    PremierPush

---
+++ main.c
@@ -0,0 +1,25 @@
+void my_print_ascii(void) {
+    for (size_t i = 0; i < count; i++)
+    {
+        void print_char(char xxx)
+    }
+    
+    
+}
+
+
+
+int main(void)
+{
+    my_print_ascii();
+return 0; 
+}
+
+
+
+
+// char type name () {}
+// print_char(char c);
+// print_number(int number);
+
+$> !"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghij klmnopqrstuvwxyz { | } ~
---
+++ my_print_ascii.c
@@ -0,0 +1,8 @@
+void my_print_ascii(void) {
+    for (size_t i = 0; i < count; i++)
+    {
+        void print_char(char xxx)
+    }
+    
+    
+}
